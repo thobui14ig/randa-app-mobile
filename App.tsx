5a0d15ada3ce930b46de7aae82e2d358abfa8ad8
thobui14ig <buithanhtho14ig@gmail.com>
2023-08-12 13:14:28 +0700

    react icon

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import { View, Button, Alert, StyleSheet } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
+import React, { useEffect } from 'react';
+import { Button, StyleSheet, View } from 'react-native';
 import Sound from './src/hooks/Sound';
 
 export default function App() {
@@ -11,6 +12,7 @@ export default function App() {
 
   return (
     <View style={styles.container}>
+      <AntDesign name="down" size={24} color="black" />
       <Button title="Phát âm thanh" onPress={playSound} />
     </View>
   );
