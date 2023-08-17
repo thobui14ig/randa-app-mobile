1cb605b302e2186a8e36f979bbb8812ca0a5e21c
thobui14ig <buithanhtho14ig@gmail.com>
2023-08-17 12:35:17 +0700

    api

--- src/api/apiConstant.ts
+++ src/api/apiConstant.ts
@@ -1,6 +1,6 @@
 const ApiConstant = {
     // BASE_API_URL: `http://192.168.1.7:3000`,
-    BASE_API_URL: `https://buithanhtho.name.vn/`,
+    BASE_API_URL: `https://buithanhtho.name.vn/api/`,
     BASE_CLIENT_URL: 'https://chungta.vanhanhhethong.com',
   
     LOGIN: 'login',
